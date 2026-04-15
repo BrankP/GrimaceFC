54f0faa3d3928194aaeafd03648ff25171863b65
BrankP <itsimple.brad@gmail.com>
2026-04-15 15:18:57 +1000

    update

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,19 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
-function resolveBase() {
-    var _a, _b;
-    var repo = (_b = (_a = process.env.GITHUB_REPOSITORY) === null || _a === void 0 ? void 0 : _a.split('/')[1]) !== null && _b !== void 0 ? _b : '';
+function resolveBase(repository) {
+    var _a;
+    var repo = (_a = repository === null || repository === void 0 ? void 0 : repository.split('/')[1]) !== null && _a !== void 0 ? _a : '';
     if (!repo)
         return '/';
-    if (repo.toLowerCase().endsWith('.github.io'))
+    if (repo.toLowerCase().slice(-10) === '.github.io')
         return '/';
     return "/".concat(repo, "/");
 }
-export default defineConfig({
-    plugins: [react()],
-    base: resolveBase(),
+export default defineConfig(function (_a) {
+    var mode = _a.mode;
+    var env = loadEnv(mode, '.', '');
+    return {
+        plugins: [react()],
+        base: resolveBase(env.GITHUB_REPOSITORY),
+    };
 });
