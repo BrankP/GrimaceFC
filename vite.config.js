c9bd2cbf1c387791d24961b8d797f6b4a896bf5e
BrankP <itsimple.brad@gmail.com>
2026-04-17 08:20:47 +1000

    push

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
+import { cloudflare } from "@cloudflare/vite-plugin";
 function resolveBase(repository) {
     var _a;
     var repo = (_a = repository === null || repository === void 0 ? void 0 : repository.split('/')[1]) !== null && _a !== void 0 ? _a : '';
@@ -13,7 +14,7 @@ export default defineConfig(function (_a) {
     var mode = _a.mode;
     var env = loadEnv(mode, '.', '');
     return {
-        plugins: [react()],
+        plugins: [react(), cloudflare()],
         base: resolveBase(env.GITHUB_REPOSITORY),
     };
 });
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
+import { cloudflare } from "@cloudflare/vite-plugin";
+
 function resolveBase(repository?: string) {
   const repo = repository?.split('/')[1] ?? '';
   if (!repo) return '/';
@@ -12,7 +14,7 @@ export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '.', '');
 
   return {
-    plugins: [react()],
+    plugins: [react(), cloudflare()],
     base: resolveBase(env.GITHUB_REPOSITORY),
   };
 });
